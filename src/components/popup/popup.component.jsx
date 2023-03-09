cad293156db251c954e8247e62092684249ddd49
ayman <aymany517@gmail.com>
2023-03-09 15:53:19 +0100

    update 2/9/2023

--- src/components/popup/popup.component.jsx
+++ src/components/popup/popup.component.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect, Fragment } from 'react';
 import apple from '../../assets/apple.png';
 import android from '../../assets/android.png';
 import star from '../../assets/star.png';
 import downloadP from '../../assets/download.png';
-
-
 import './popup.styles.scss';
 
+import settings from "../../assets/settings.png";
+
+
+
+let interval = undefined;
 
 const Popup =({ popup, Close }) => {
-    const { urlImage, rating, download, author, title, paragraph, Link_Download, Link_LoadingPage } = popup;
+    const { urlImage, rating, download, author, title, paragraph, Link_Download } = popup;
+    const [verificationToggle , setVerificationToggle ] = useState(false);
+
+    const [running, setRunning] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [des, setDes] = useState("");
+
+    const res = title.replace(/ /g, '')
 
     // eslint-disable-next-line no-unused-vars
     function CPABuildComplete(){
@@ -21,8 +32,43 @@ const Popup =({ popup, Close }) => {
         CPABuildLock();
     }
 
+    useEffect(() => {
+        if (running) {
+            interval = setInterval(() => {
+            setProgress((prev) => prev + 1);
+        }, 100);
+        } else {
+            clearInterval(interval);
+        }
+    }, [running]);
+    
+    useEffect(() => {
+        if (progress === 100) {
+            setRunning(false);
+            clearInterval(interval);
+        }
+    }, [progress]);
+
+    useEffect(() => {
+        if (progress === 1) {
+            setDes(`Downloading ${res}.gz`);
+        } if (progress === 30) {
+            setDes(`Unpacking ${res}.gz`);
+        } if (progress === 80) {
+            setDes("starting injection...");
+        } if (progress === 98) {
+            setDes("setting up final stage...");
+        } if (progress === 100) {
+            setTimeout(()=>{
+                locker();
+            }, 1000);
+        }
+    }, [progress, res]);
+
+
     return (
-        <div className="apps__popup" onClick={Close}>
+        <div className="apps__popup">
+
             <div className="apps__popup__body">
                 <p className="apps__popup__exit" onClick={Close}>X</p>
 
@@ -69,13 +115,48 @@ const Popup =({ popup, Close }) => {
 
                     <div className='apps__popup__details--download'>
                         <h1 className='apps__popup__details--download--heading'>Download ready</h1>
-                        <p className='apps__popup__details--download--paragraph'>Click the buttons below in order to get your coins or start with your app download</p>
-                        <p className='apps__popup__details--download--link' onClick={()=> { window.open(Link_LoadingPage, "_blank"); }}>GET YOUR COINS</p>
-                        <p className='secondary' onClick={locker}>DOWNLOAD NOW</p>
+                        <p className='apps__popup__details--download--paragraph'>Click the button below in order to start with your app download</p>
+                        
+                        <p className='apps__popup__details--download--link' onClick={() => {
+                                                                                        setVerificationToggle (!verificationToggle);
+                                                                                        setRunning(true);
+                                                                                        setProgress(0);
+
+                                                                                    }}>
+                        DOWNLOAD NOW
+                    </p>
                     </div>
                 </div>
             </div>
+            {verificationToggle&& 
+                        <div className="valid">
+                            <div className="valid__body">
+                                <img
+                                    src={settings}
+                                    alt="settings"
+                                    className="valid__body__image"
+                                />
+                                {progress<100 ? 
+                                    <Fragment>
+                                        <p className="valid__body__title">PROCCESSING...</p>
+                                        <p className="valid__body__description" style={{ fontSize : progress >= 80 ? '1.8rem' : '2rem', width : progress >= 80 ? '24rem' : '16rem'}}>{des}</p>
+                                        <div className="valid__body__progress">
+                                            <div className="valid__body__progress__valid" style={{width: `${progress}%`}}></div>
+                                        </div>
+                                    </Fragment>:
+
+                                    <Fragment>
+                                        <p className="valid__body__title__waiting">Waiting...</p>
+                                    </Fragment>
+                                }
+                                
+                            </div>
+                        </div>
+            }
         </div>
     );
 }
-export default Popup;
+export default Popup;  
+
+
+/* <p className='secondary' onClick={locker}>DOWNLOAD NOW</p> */
